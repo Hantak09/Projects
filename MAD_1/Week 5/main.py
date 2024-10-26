c90682c789dc84d49bdbaa022c548fd9ad315d9c
Hantak09 <harsh.bairagi.05@gmail.com>
2024-10-27 00:57:24 +0530

    Frontend added

--- MAD_1/Week 5/main.py
+++ MAD_1/Week 5/main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template_string
 from flask import request
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -20,27 +20,48 @@ class Student(db.Model):
     last_name = db.Column(db.String)
 
 class Course(db.Model):
-    # __tablename__ = 'course'
+    __tablename__ = 'course'
     course_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     course_code = db.Column(db.String, unique=True, nullable=False)
     course_name = db.Column(db.String, nullable=False)
     course_description = db.Column(db.String)
 
 class Enrollments(db.Model):
-    # __tablename__ = 'enrollments'
+    __tablename__ = 'enrollments'
     enrollment_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     estudent_id = db.Column(db.Integer, db.ForeignKey('student.student_id'), nullable=False)
     ecourse = db.Column(db.Integer, db.ForeignKey('course.course_id'), nullable=False)
+
+def check_roll_no_exists(roll_no: str) -> bool:
+    result = Student.query.where(Student.roll_number == roll_no).one_or_none()
+    if result is None:
+        return False
+    else:
+        return True
 
 @app.route('/')
 def index():
     students = Student.query.all()
     if len(students) == 0:
         return render_template("empty_index.html")
+    else:
+        return render_template("index.html", students=students)
 
 @app.route('/student/create', methods=['GET', 'POST'])
 def add_student():
-    return render_template("add_student.html")
+    if request.method == 'GET':
+        return render_template("add_student.html")
+    elif request.method == 'POST':
+        """Extracting Form Data"""
+        roll_number = request.form['roll']
+        first_name = request.form['f_name']
+        last_name = request.form['l_name']
+        checkbox = request.form.getlist("courses")
+
+        if check_roll_no_exists(roll_number):
+            return render_template("student_exist.html")
+        else:
+            return render_template_string("testing")
 
 
 if __name__=='__main__':
