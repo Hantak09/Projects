af4473d1caa9137f2a33cda573eae248b5803c7d
Hantak09 <harsh.bairagi.05@gmail.com>
2024-11-04 00:45:26 +0530

    Files updated

---
+++ MAD_1/Week 6/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
