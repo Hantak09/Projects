296a0214d55f310d9b954e3ef9f7961cd523f32a
Hantak09 <harsh.bairagi.05@gmail.com>
2024-11-09 18:37:33 +0530

    Files updated

--- MAD_1/Week 6/main.py
+++ MAD_1/Week 6/main.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask_restful import Api
 from source.database import db
+
 
 def create_app():
     server = Flask(__name__)
     server.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///api_database.sqlite3'
+    api = Api(server)
     db.init_app(server)
     server.app_context().push()
-    return server
+    return server, api
 
 app = create_app()
 
--- MAD_1/Week 6/source/controllers.py
+++ MAD_1/Week 6/source/controllers.py
@@ -1,3 +1,10 @@
 from flask import request
 from flask import make_response
 
+class USER_CONTROLLER:
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
